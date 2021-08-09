06ad33e76e5c6ada31dc9416c9a26df0070bc5b6
alex <alexjelescu@gmail.com>
2021-08-09 22:00:17 +0300

    Implemented validator for productDto

--- src/main/java/com/sda/onlineAuction/service/ProductService.java
+++ src/main/java/com/sda/onlineAuction/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.sda.onlineAuction.service;
 
 import com.sda.onlineAuction.dto.ProductDto;
+import com.sda.onlineAuction.model.Category;
 import com.sda.onlineAuction.model.Product;
 import com.sda.onlineAuction.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class ProductService {
         Product product = new Product();
         product.setName(productDto.getName());
         product.setDescription(productDto.getDescription());
+        product.setCategory(Category.valueOf(productDto.getCategory()));
         product.setStartingPrice(Integer.valueOf(productDto.getStartBiddingPrice()));
         product.setEndDateTime(LocalDateTime.parse(productDto.getEndDateTime()));
         productRepository.save(product);
---
+++ src/main/java/com/sda/onlineAuction/validator/ProductDtoValidator.java
@@ -0,0 +1,25 @@
+package com.sda.onlineAuction.validator;
+
+import com.sda.onlineAuction.dto.ProductDto;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Service
+public class ProductDtoValidator {
+
+    public void validate(ProductDto productDto, BindingResult bindingResult) {
+        String priceAsString = productDto.getStartBiddingPrice();
+        try {
+            Integer priceAsInteger = Integer.valueOf(priceAsString);
+            if (priceAsInteger <= 0) {
+                FieldError fieldError = new FieldError("productDto", "startBiddingPrice", "Price must be positive!");
+                bindingResult.addError(fieldError);
+            }
+        } catch (NumberFormatException numberFormatException) {
+            FieldError fieldError = new FieldError("productDto", "startBiddingPrice", "Price must be a number!");
+            bindingResult.addError(fieldError);
+        }
+
+    }
+}
